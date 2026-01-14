ef89e3c6b3cd26f6f05b34a7e87e0129dd2691b8
ali-devspark <ali.devspark@gmail.com>
2026-01-14 13:00:06 +0300

    initial commit

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="min-w-screen min-h-screen">
+      <h1 className="text-3xl font-bold text-center mt-16 p-5 w-10/12 mx-auto bg-indigo-800 rounded-xl shadow-lg shadow-indigo-500 dark:text-amber-600">
+        Home
+      </h1>
+    </div>
+  );
+}
